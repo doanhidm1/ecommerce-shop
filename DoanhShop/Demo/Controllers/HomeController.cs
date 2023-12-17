bc3b3c361a4c9eec3a86acf0cd6074e9fccf5dd8
doanh20cm <doanhidm1@gmail.com>
2023-12-17 22:05:57 +0700

    add hello action to home controller

--- DoanhShop/Demo/Controllers/HomeController.cs
+++ DoanhShop/Demo/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Demo.Controllers
             return View();
         }
 
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
